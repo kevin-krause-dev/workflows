2b42057a31a7271d52a8f5dd4dfe28e3be419323
kevin-krause <contatokevin-krause@gmail.com>
2025-02-18 08:08:57 -0300

    coverage +80%

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -62,12 +62,18 @@ describe('factorial', () => {
     it('should return the factorial of a number correctly', () => {
         expect(factorial(5)).toBe(120)
         expect(factorial(0)).toBe(1)
+        expect(() => factorial(-1)).toThrow(
+            'Factorial is not defined for negative numbers'
+        )
     })
 })
 
 describe('fibonacci', () => {
     it('should return the fibonacci of a number correctly', () => {
         expect(fibonacci(5)).toBe(5)
+        expect(() => fibonacci(-1)).toThrow(
+            'Fibonacci is not defined for negative numbers'
+        )
     })
 })
 
